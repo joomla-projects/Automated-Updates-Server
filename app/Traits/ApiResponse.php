b4331a6736230334afbe4a3522f06d5e278c171b
Robert Deutz <rdeutz@googlemail.com>
2024-11-16 14:07:55 +0100

    stan fix

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -2,24 +2,52 @@
 
 namespace App\Traits;
 
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Api Response
+ */
 trait ApiResponse
 {
-    protected function ok($message = 'OK')
+    /**
+     * @param $message
+     *
+     * @return JsonResponse
+     */
+    protected function ok($message = 'OK'): JsonResponse
     {
         return $this->success($message, 200);
     }
 
-    protected function success($message, $statusCode = 200)
+    /**
+     * @param $message
+     * @param $statusCode
+     *
+     * @return JsonResponse
+     */
+    protected function success($message, $statusCode = 200): JsonResponse
     {
         return $this->response($message, $statusCode);
     }
 
-    protected function error($message, $statusCode = 400)
+    /**
+     * @param $message
+     * @param $statusCode
+     *
+     * @return JsonResponse
+     */
+    protected function error($message, $statusCode = 400): JsonResponse
     {
         return $this->response($message, $statusCode);
     }
 
-    protected function response($message, $statusCode = 200)
+    /**
+     * @param $message
+     * @param $statusCode
+     *
+     * @return JsonResponse
+     */
+    protected function response($message, $statusCode = 200): JsonResponse
     {
         return response()->json([
             'message' => $message,
