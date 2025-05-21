cb3ebd9c5b5113d00404466f69620793838229cc
David Jardin <d.jardin@djumla.de>
2025-05-21 16:20:43 +0200

    implement rate limiting

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\V1\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware(['throttle:site'])->group(function () {
     Route::controller(SiteController::class)->group(function () {
         Route::post('register', 'register');
         Route::post('check', 'check');
