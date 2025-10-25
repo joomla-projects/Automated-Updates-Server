34e4958c2a19e2e06ead6181543c09ae1946bf18
David Jardin <d.jardin@djumla.de>
2025-10-25 20:21:10 +0200

    added oh-dear-compatible healthcheck endpoint

---
+++ app/Providers/HealthCheckProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+use Spatie\Health\Facades\Health;
+
+class HealthCheckProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Health::checks([
+            UsedDiskSpaceCheck::new(),
+            DatabaseCheck::new(),
+            HorizonCheck::new()
+        ]);
+    }
+}
